a8b3ae5998508ad13712e2cdd88c9eb5d21d2611
Alexandre Angelim <angelim@angelim.com.br>
2016-05-31 14:45:58 -0300

    suppresses Regexp warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+# Snowplow uses questionable regex to validate the iglu location as it doesn't scape -
+# Setting verbose to nil supresses a lot of warnings emmited by the Regexp class.
+$VERBOSE = nil
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'snowly'
 require 'snowplow-tracker'
